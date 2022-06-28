6273ec8abf32c0326733061434c508a6f21af642
Kerem <47521147+keremtigre@users.noreply.github.com>
2022-06-28 19:55:45 +0300

    Update admob_helper.dart

--- lib/core/admob/admob_helper.dart
+++ lib/core/admob/admob_helper.dart
@@ -1,10 +1,11 @@
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AddmobService {
-  static String get bannerAdUnitId => "ca-app-pub-9703444831349835/8769535717";
+  static String get bannerAdUnitId => "ca-app-pub-9703444831349835/8769";
   static String get testbannerAdUnitId =>
       "ca-app-pub-3940256099942544/6300978111";
   static initialize() {
+    //brd535717
     if (MobileAds.instance == null) {
       MobileAds.instance.initialize();
     }
